bbe08288d99160c03a63d7e7bd4d6c8689035867
김다은 <softeun92@naver.com>
2021-11-21 12:08:38 +0900

    메인화면 UI + 토큰필요 (sharedPrefer ) 저장

--- app/src/main/java/com/example/keepthetime_20211121/LoginActivity.kt
+++ app/src/main/java/com/example/keepthetime_20211121/LoginActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.keepthetime_20211121.databinding.ActivityLoginBinding
 import com.example.keepthetime_20211121.datas.BasicResponse
+import com.example.keepthetime_20211121.utils.ContextUtil
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -52,9 +53,8 @@ class LoginActivity : BaseActivity() {
 
 
                         val basicResponse = response.body()!!
-                        Log.d("로그인성공",basicResponse.message)
 
-                        Log.d("사용자토큰",basicResponse.data.token)
+                        ContextUtil.setToken(mContext,basicResponse.data.token)
 
 
                         Toast.makeText(mContext, "${basicResponse.data.user.nickname}님 환영합니다!",
