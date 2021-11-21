92b38fb81cfa29dc6b50f96deafc32f74c22668a
김다은 <softeun92@naver.com>
2021-11-21 10:41:19 +0900

    레트로핏 인터페이스 추가   - 로그인 기능 예시

---
+++ app/src/main/java/com/example/keepthetime_20211121/api/ServerAPIService.kt
@@ -0,0 +1,18 @@
+package com.example.keepthetime_20211121.api
+
+import retrofit2.http.Field
+import retrofit2.http.POST
+
+interface ServerAPIService {
+
+//    기능별 주소 (endpoint) / 메쏘드 (POST) / 파라미터 명시.  <=  이것만 명시하는 별도의 문서
+
+
+    @POST("/user")
+    fun postRequestLogin(
+        @Field("email") email: String,
+        @Field("password")  pw : String
+    )
+
+
+}
