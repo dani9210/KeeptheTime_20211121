008e7556f4f13a03b243923d281bdc5189ab3538
김다은 <softeun92@naver.com>
2021-11-21 13:40:08 +0900

    로그인에 실패한경우 - JSONObject 직접 생성 /파싱

--- app/src/main/java/com/example/keepthetime_20211121/LoginActivity.kt
+++ app/src/main/java/com/example/keepthetime_20211121/LoginActivity.kt
@@ -8,6 +8,7 @@ import androidx.databinding.DataBindingUtil
 import com.example.keepthetime_20211121.databinding.ActivityLoginBinding
 import com.example.keepthetime_20211121.datas.BasicResponse
 import com.example.keepthetime_20211121.utils.ContextUtil
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -69,6 +70,17 @@ class LoginActivity : BaseActivity() {
 
 
 
+                    }
+
+                    else{
+
+//                      로그인 결과 실패. => 실패사유 (message) 토스트
+
+                        val errorBodyStr = response.errorBody()!!.string()
+                        val jsonObj = JSONObject(errorBodyStr)
+
+                        val message = jsonObj.getString("message")
+                        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
                     }
 
 
