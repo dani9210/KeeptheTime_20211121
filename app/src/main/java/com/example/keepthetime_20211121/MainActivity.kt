eee81b5558b65b02d03b6c1d3fdb4701786a0946
김다은 <softeun92@naver.com>
2021-11-21 10:17:11 +0900

    로그인 화면 UI추가

--- app/src/main/java/com/example/keepthetime_20211121/MainActivity.kt
+++ app/src/main/java/com/example/keepthetime_20211121/MainActivity.kt
@@ -12,6 +12,8 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
 
     override fun setupEvents() {
