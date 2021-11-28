89f79954e349ef08bedab5b9ab3855d40ab1e7a8
김다은 <softeun92@naver.com>
2021-11-28 12:04:07 +0900

    내 친구 목록 API 호출

--- app/src/main/java/com/example/keepthetime_20211121/ViewFriendListActivity.kt
+++ app/src/main/java/com/example/keepthetime_20211121/ViewFriendListActivity.kt
@@ -4,10 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.keepthetime_20211121.databinding.ActivityViewFriendListBinding
+import com.example.keepthetime_20211121.datas.BasicResponse
+import com.example.keepthetime_20211121.datas.UserData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ViewFriendListActivity : BaseActivity() {
 
     lateinit var binding : ActivityViewFriendListBinding
+
+    val mMyFriendList = ArrayList<UserData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_view_friend_list)
@@ -21,6 +29,36 @@ class ViewFriendListActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        getMyFriendServer()
 
     }
+
+
+    fun getMyFriendServer(){
+
+        apiService.getRequestMyFriends("my").enqueue(object : Callback<BasicResponse>{
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                if ( response.isSuccessful){
+
+                    val br = response.body()!!
+
+
+
+                }
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+
+        })
+
+
+
+
+    }
+
 }
