3ed84086782e44d98f6035b8735e60e8a3527c71
김다은 <softeun92@naver.com>
2021-11-28 13:59:27 +0900

    GlobalData 클래스에, 로그인한 사용자 정보 저장.   - 자동로그인 : 저장된 토큰으로 내정보 불러오기 시도 => GlobalData의 로그인 유져에 저장되는지?

--- app/src/main/java/com/example/keepthetime_20211121/SplashActivity.kt
+++ app/src/main/java/com/example/keepthetime_20211121/SplashActivity.kt
@@ -5,7 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
+import com.example.keepthetime_20211121.datas.BasicResponse
 import com.example.keepthetime_20211121.utils.ContextUtil
+import com.example.keepthetime_20211121.utils.GlobalData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,10 +31,14 @@ class SplashActivity : BaseActivity() {
         myHandler.postDelayed( {
 
             val myIntent : Intent
+0
+//            자동로그인 할 상황 인지 검사.  =>  내가 갖고있는 토큰이, 유효한 토큰인가?
+//            내 정보 API를 통해서 -> 사용자 정보가 저장되었는가?
 
-//            자동로그인 할 상황 인지 검사.
 
-            if (ContextUtil.getToken(mContext) == ""){
+            if (GlobalData.loginUser == null){
+
+//                토큰 없거나 or 있지만 유효하지 않으면  => 로그인
 
 //                토큰 없는 상황 : 로그인 필요
 
@@ -53,6 +63,41 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        testToken()
+
+
+    }
+
+    fun testToken()  {
+
+//        ContextUtil에 저장된 토큰이 괜찮은토큰인가?  검사 -> 내 정보 불러오기. 잘불러오는지 확인.
+
+        apiService.getRequestMyInfo().enqueue(object : Callback<BasicResponse>{
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                if (response.isSuccessful){
+
+                    val br = response.body()!!
+
+//                    Globaldata의 로그인 자용자에 사용자 정보를 저장.
+
+                    GlobalData.loginUser = br.data.user
+
+                }
+
+                else{
+
+
+                }
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+
+        })
 
     }
 }
