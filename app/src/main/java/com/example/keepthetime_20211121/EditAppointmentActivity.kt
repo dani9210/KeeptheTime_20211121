9974dc24479f9d421b7a6a427f651df420d46f2e
김다은 <softeun92@naver.com>
2021-11-28 16:12:51 +0900

    임시 약속등록 API 실행

--- app/src/main/java/com/example/keepthetime_20211121/EditAppointmentActivity.kt
+++ app/src/main/java/com/example/keepthetime_20211121/EditAppointmentActivity.kt
@@ -2,8 +2,13 @@ package com.example.keepthetime_20211121
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.example.keepthetime_20211121.databinding.ActivityEditAppointmentBinding
+import com.example.keepthetime_20211121.datas.BasicResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class EditAppointmentActivity : BaseActivity() {
 
@@ -16,6 +21,36 @@ class EditAppointmentActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        binding.btnOk.setOnClickListener {
+
+            Log.d("눌렸다",)
+
+            val inputTitle = binding.edtTitle.text.toString()
+            val inputDataTime = binding.edtDateTime.text.toString()
+            val inputPlace = binding.edtPlace.text.toString()
+            val inputLat = binding.edtLatitude.text.toString().toDouble()
+            val inputLng = binding.edtLongitude.text.toString().toDouble()
+
+            apiService.postRequestAppointment(inputTitle,inputDataTime,inputPlace,inputLat,inputLng).enqueue(object : Callback<BasicResponse>{
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+
+
+
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                }
+
+
+            })
+
+
+        }
 
     }
 
