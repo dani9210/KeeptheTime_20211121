aa35f39f1f8c455b39a6916ea8d39874647bb285
김다은 <softeun92@naver.com>
2021-12-05 13:58:18 +0900

    기능 구현 - 지도를 클릭하면, 클릭한 위치 (위도, 경도)  ,표현할 마커 위치 이동   - 처음 클릭하면 새로 마커 생성

--- app/src/main/java/com/example/keepthetime_20211121/EditAppointmentActivity.kt
+++ app/src/main/java/com/example/keepthetime_20211121/EditAppointmentActivity.kt
@@ -28,6 +28,13 @@ class EditAppointmentActivity : BaseActivity() {
 
     val mSelectedDateTime = Calendar.getInstance()      // 기본값 : 현재 일시
 
+//    약속을 잡을 위치를 저장할 변수 (네이버 - LatLng)
+//    그 위치를 보여줄 마커 (네이버 - Marker)
+//    처음 화면이 나타날때,아직 선택 안한상태. => 위치도 / 마커도 아직 없다. (초기 값 - null)
+
+    var mSelectedLatLng : LatLng? = null
+    var mSelectedMarker : Marker? = null
+
     lateinit var binding : ActivityEditAppointmentBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -232,14 +239,26 @@ class EditAppointmentActivity : BaseActivity() {
             naverMap.setOnMapClickListener { point, latLng ->
 
 
+
 //            클릭된 좌표  latLng -> 카메라이동 (정가운데)  /  마커찍기
 
                 val cameraUpdate = CameraUpdate.scrollTo(latLng)
                 naverMap.moveCamera(cameraUpdate)
 
-                val marker = Marker()
-                marker.position = latLng
-                marker.map = naverMap
+//               선택 한 위치를 멤버변수에 담아두자.
+
+                mSelectedLatLng = latLng
+
+//                선택 한 위치르 보여줄 마커도 (만들어진게 없다면 새로) 생성.
+
+                if(mSelectedMarker == null ){
+
+                    mSelectedMarker = Marker()
+
+                }
+
+                mSelectedMarker!!.position = latLng
+                mSelectedMarker!!.map = naverMap
 
 
             }
