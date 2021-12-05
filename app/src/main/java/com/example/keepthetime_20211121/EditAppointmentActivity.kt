b9ed95d95effbd618b5053b898d9035c38bba70f
김다은 <softeun92@naver.com>
2021-12-05 10:45:41 +0900

    Calendar 클래스 활용 (2)   - 연도 / 월 / 일을 한번에 저장하는 set 기능 활용

--- app/src/main/java/com/example/keepthetime_20211121/EditAppointmentActivity.kt
+++ app/src/main/java/com/example/keepthetime_20211121/EditAppointmentActivity.kt
@@ -1,16 +1,24 @@
 package com.example.keepthetime_20211121
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.DatePicker
 import androidx.databinding.DataBindingUtil
 import com.example.keepthetime_20211121.databinding.ActivityEditAppointmentBinding
 import com.example.keepthetime_20211121.datas.BasicResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.*
 
 class EditAppointmentActivity : BaseActivity() {
+
+
+//    약속을 잡을 일시를 저장할 변수(Calendar)
+
+    val mSelectedDateTime = Calendar.getInstance()      // 기본값 : 현재 일시
 
     lateinit var binding : ActivityEditAppointmentBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,18 +30,68 @@ class EditAppointmentActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        binding.txtDate.setOnClickListener {
+
+//            날짜 선택 팝업 (DatePickerDialog)  사용 예시
+
+//            선택 완료 시 할 일 (JAVA - Interface 설정 => 변수에 담아두자
+
+            val dateSetListener =  object : DatePickerDialog.OnDateSetListener{
+                override fun onDateSet(p0: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+//                    실제로 날짜가 선택되면 할 일 적는 공간.
+
+//                    Log.d("선택한년월일","${year}년 ${month}월 ${dayOfMonth}일 선택됨")
+
+//                    선택된 일시를 저장할 변수에 ,  연/ 월/ 일 을저장
+
+//                    mSelectedDateTime.set(Calendar.YEAR,year)
+//                    mSelectedDateTime.set(Calendar.MONTH,month)
+//                    mSelectedDateTime.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+
+//                    년/ 월/일 을 한번에 저장하는 set 함수 활용
+
+
+                    mSelectedDateTime.set(year,month,dayOfMonth)
+
+                }
+
+            }
+
+//            실제 팝업창 띄우기
+
+//            Kotlin : JAVA 기반 언어 => 월 : 0 ~ 11로 만들어져있음.
+
+//            오늘 날짜 기본으로 띄우도록. => mSelectedDateTime에 저장된 값 활용
+
+            val datePickerDialog = DatePickerDialog(
+                mContext,
+                dateSetListener,
+                mSelectedDateTime.get(Calendar.YEAR),
+                mSelectedDateTime.get(Calendar.MONTH),
+                mSelectedDateTime.get(Calendar.DAY_OF_MONTH)
+            )
+
+            datePickerDialog.show()
+
+
+        }
+
+
+
+
         binding.btnOk.setOnClickListener {
 
 
 
             val inputTitle = binding.edtTitle.text.toString()
-            val inputDataTime = binding.edtDateTime.text.toString()
+//            val inputDataTime = binding.edtDateTime.text.toString()
             val inputPlace = binding.edtPlace.text.toString()
             val inputLat = binding.edtLatitude.text.toString().toDouble()
             val inputLng = binding.edtLongitude.text.toString().toDouble()
 
 
-            apiService.postRequestAppointment(inputTitle,inputDataTime,inputPlace,inputLat,inputLng).enqueue(object : Callback<BasicResponse>{
+            apiService.postRequestAppointment(inputTitle,"임시값",inputPlace,inputLat,inputLng).enqueue(object : Callback<BasicResponse>{
                 override fun onResponse(
                     call: Call<BasicResponse>,
                     response: Response<BasicResponse>
