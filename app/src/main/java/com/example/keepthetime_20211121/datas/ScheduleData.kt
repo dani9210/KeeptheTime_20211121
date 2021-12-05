fa45429a7bd5f597c6927a6553ab51c9011bf37b
김다은 <softeun92@naver.com>
2021-12-05 15:16:07 +0900

    스케쥴 데이터의 기능 추가   - Date로 파싱된 약속 일시를 다시 새로운 양식의 String으로 재가공해서 내보내주는 기능

--- app/src/main/java/com/example/keepthetime_20211121/datas/ScheduleData.kt
+++ app/src/main/java/com/example/keepthetime_20211121/datas/ScheduleData.kt
@@ -1,6 +1,7 @@
 package com.example.keepthetime_20211121.datas
 
 import com.google.gson.annotations.SerializedName
+import java.text.SimpleDateFormat
 import java.util.*
 
 class ScheduleData(
@@ -14,4 +15,22 @@ class ScheduleData(
     @SerializedName("created_at")
     var createdAt : String
 ) {
+
+
+
+
+
+    fun getFormattedDatetime( ) : String {
+
+        //    datetime ( 약속일시) : Date로 저장되어 있다.
+//    기능 추가 -> Data = > 가공된 String으로 내보내주는 함수.
+
+        val twoLineFormat = SimpleDateFormat( "M월 d일\na h:mm" )
+
+        return twoLineFormat.format(this.dateTime)
+
+
+    }
+
+
 }
