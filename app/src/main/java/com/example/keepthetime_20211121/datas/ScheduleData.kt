64b4c23ce6ab6f0291eb45a593f69217be429272
김다은 <softeun92@naver.com>
2021-12-05 14:19:33 +0900

    일정 정보를 파싱하는 어댑터

---
+++ app/src/main/java/com/example/keepthetime_20211121/datas/ScheduleData.kt
@@ -0,0 +1,14 @@
+package com.example.keepthetime_20211121.datas
+
+class ScheduleData(
+
+    var id : Int,
+    var title : String,
+    var dateTime : String,
+    var place : String,
+    var latitude : Double,
+    var longitude : Double,
+    @Serializedname("created_at")
+    var createdAt : String
+) {
+}
