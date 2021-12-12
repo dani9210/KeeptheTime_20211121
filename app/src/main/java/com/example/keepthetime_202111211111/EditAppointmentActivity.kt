eaa36b844e7c62b5c47c887a95d8eed7a5bec1c3
김다은 <softeun92@naver.com>
2021-12-12 15:04:04 +0900

    커스텀뷰 내부의 리싸이클러뷰 액티비티에서 추출

--- app/src/main/java/com/example/keepthetime_202111211111/EditAppointmentActivity.kt
+++ app/src/main/java/com/example/keepthetime_202111211111/EditAppointmentActivity.kt
@@ -10,6 +10,7 @@ import android.widget.TimePicker
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
 import com.example.keepthetime_202111211111.databinding.ActivityEditAppointmentBinding
 import com.example.keepthetime_202111211111.datas.BasicResponse
 import com.example.keepthetime_202111211111.datas.PlaceData
@@ -143,6 +144,12 @@ class EditAppointmentActivity : BaseActivity() {
                         val alert = AlertDialog.Builder(mContext)
 
                         val customView = LayoutInflater.from(mContext).inflate(R.layout.my_custom_alert_select_place,null)
+
+//                        커스텀뷰 안의 리싸이클러뷰 추출
+
+                        val placeSelectRecyclerView = customView.findViewById<RecyclerView>(R.id.placeSelectRecyclerView)
+
+//                        리싸이클러뷰에, placeList에 담긴 장소목록을 표시.
 
                         alert.setTitle("약속 장소 선택")
 //                        alert.setMessage("정말~~?")
