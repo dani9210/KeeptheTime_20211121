561972d7a707b10f4df2736461aa650df11b0e3c
김다은 <softeun92@naver.com>
2021-12-12 15:42:18 +0900

    실제 어댑터의 아이템 클릭시 할일 지정\   - 받아낸 장소 데이터의 이름 토스트 출력

--- app/src/main/java/com/example/keepthetime_202111211111/EditAppointmentActivity.kt
+++ app/src/main/java/com/example/keepthetime_202111211111/EditAppointmentActivity.kt
@@ -153,9 +153,21 @@ class EditAppointmentActivity : BaseActivity() {
 
 //                        리싸이클러뷰에, placeList에 담긴 장소목록을 표시.
 
-                        val PlaceAdapter = PlaceSelectRecyclerAdapter(mContext,placeList)
-                        placeSelectRecyclerView.adapter = PlaceAdapter
+                        val placeAdapter = PlaceSelectRecyclerAdapter(mContext,placeList)
+                        placeSelectRecyclerView.adapter = placeAdapter
                         placeSelectRecyclerView.layoutManager = LinearLayoutManager(mContext)
+
+//                        만든 리싸이클러 어댑터의 변수로 들어있는 onItemClick 기능 활용.
+
+                        placeAdapter.onItemClickListener  = object  : PlaceSelectRecyclerAdapter.OnItemClickListener{
+                            override fun onItemClick(data: PlaceData) {
+
+                                Toast.makeText(mContext, data.placeName, Toast.LENGTH_SHORT).show()
+
+                            }
+
+
+                        }
 
                         alert.setTitle("약속 장소 선택")
 //                        alert.setMessage("정말~~?")
