df329aaf0558c236aa663437b704810ea6a59da6
김다은 <softeun92@naver.com>
2021-12-19 13:35:28 +0900

    스피너 모드 변경 예시 (다이얼 로그)

--- app/src/main/java/com/example/keepthetime_202111211111/adapters/StartingPointSpinnerAdapter.kt
+++ app/src/main/java/com/example/keepthetime_202111211111/adapters/StartingPointSpinnerAdapter.kt
@@ -15,9 +15,29 @@ class StartingPointSpinnerAdapter(
     val mList : List<PlaceData>
     ) : ArrayAdapter<PlaceData>(mContext,resId,mList) {
 
-//    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-//
-//    }
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if(tempRow == null) {
+
+
+//            돌려막기 할 만큼 충분한 row가 그려지지 않은 상태.
+//            mInflater 변수 대신, 바로 LayoutInflater.from 기능 활용
+
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.starting_point_list_item,null)
+
+        }
+
+        val row = tempRow!!
+        val data = mList[position]
+        val txtPlaceName = row.findViewById<TextView>(R.id.txtPlaceName)
+
+        txtPlaceName.text= data.placeName
+
+        return row
+
+    }
 
     override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
 
