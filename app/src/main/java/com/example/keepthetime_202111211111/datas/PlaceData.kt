ce525e8760ef34ad6dc1f114ef7c25834bb63437
김다은 <softeun92@naver.com>
2021-12-19 12:14:45 +0900

    장소데이터 클래스 (PlaceData) 수정   - id 변수 추가 : 기존 생성자 사용 코드에 임시 id 값 대입   - 서버는 name 이름표로 장소 이름 알려줌. : @SeralizedName을 통해 name 이름표 => placeName 변수에 대입

--- app/src/main/java/com/example/keepthetime_202111211111/datas/PlaceData.kt
+++ app/src/main/java/com/example/keepthetime_202111211111/datas/PlaceData.kt
@@ -2,6 +2,8 @@ package com.example.keepthetime_202111211111.datas
 
 class PlaceData(
 
+    var id : Int,
+    @SerializedName("name")
     var placeName : String,
     var latitude : Double,
     val longitude: Double,
